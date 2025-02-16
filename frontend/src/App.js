f61b3a49d465c65b2d6799bc723f13a0a72928b9
Areeba Fazal <areeba.fazalxx11@gmail.com>
2025-02-16 03:40:05 +0000

    map colour editor working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
           [[1, 1, 0], [0, 1, 1], [1, 0, 1]], 
           [[1, 1, 1], [0, 0, 0], [1, 1, 1]], 
         ],
+        selectedColor: [1, 1, 1],
     }
 
     componentDidMount() {
@@ -31,27 +32,69 @@ class App extends React.Component {
         .catch(err => {})
 
       }
+    
+    // create a new grid with the changed colour at the index that was clicked on 
+    handleCellClick = (rowIndex, colIndex) => {
+        const newGrid = this.state.gridData.map((row, rIdx) =>
+            row.map((cell, cIdx) =>
+                rIdx === rowIndex && cIdx === colIndex ? this.state.selectedColor : cell
+            )
+        );
+        this.setState({ gridData: newGrid });
+    };
+
+    // save the colour that is picked
+    handleColorSelect = (color) => {
+        this.setState({ selectedColor: color });
+    };
 
   render() {
+    const colorOptions = [
+      {color: [0, 0, 0], name: "Off" },
+      {color: [1, 0, 0], name: "Red" },
+      {color: [0, 1, 0], name: "Green" },
+      {color: [0, 0, 1], name: "Blue" },
+      {color: [1, 1, 0], name: "Yellow" },
+      {color: [0, 1, 1], name: "Cyan" },
+      {color: [1, 0, 1], name: "Magenta" },
+      {color: [1, 1, 1], name: "White" },
+  ];
     return(
       <div>
           aaaa
             {this.state.details.map((detail, id) =>  (
             <div key={id}>
             <div >
-                  <div >
-                        <h1>{detail.detail} </h1>
-                        <footer >--- by
-                        <cite title="Source Title">
-                        {detail.name}</cite>
-                        </footer>
-                  </div>
+                <div >
+                    <h1>{detail.detail} </h1>
+                    <footer >--- by
+                    <cite title="Source Title">
+                    {detail.name}</cite>
+                    </footer>
+                </div>
             </div>
             </div>
             )
         )}
 
-      <Grid data={this.state.gridData} />
+      <h3>Select colour to edit map</h3>
+      <div style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
+          {colorOptions.map(({ color, name }) => (
+              <button
+                  key={name}
+                  onClick={() => this.handleColorSelect(color)}
+                  style={{
+                      width: "20px",
+                      height: "15px",
+                      backgroundColor: `rgb(${color[0] * 255}, ${color[1] * 255}, ${color[2] * 255})`,
+                      border: this.state.selectedColor === color ? "3px solid black" : "1px solid gray",
+                      cursor: "pointer",
+                  }}
+              ></button>
+          ))}
+          </div>
+      <h3>Map</h3>
+      <Grid data={this.state.gridData} onCellClick={this.handleCellClick} />
       </div>
       
       );
--- frontend/src/Components/Grid.jsx
+++ frontend/src/Components/Grid.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Grid = ({ data }) => {
+const Grid = ({ data, onCellClick }) => {
   return (
 
     // for each row
@@ -12,29 +12,35 @@ const Grid = ({ data }) => {
         gap: "2px",
       }}>
       
-      {data.flat().map((rgb, index) => {
-        // convert from 1 to 255 to display colour
-        const r = rgb[0] * 255;
-        const g = rgb[1] * 255;
-        const b = rgb[2] * 255;
-
-        // get colour from [rbg[0], rbg[1], rbg[2]]
-        const backgroundColor = `rgb(${r}, ${g}, ${b})`;
-
-        // for each cell in a row
-        return (
-          <div
-            key={index}
-            style={{
-              width: "50px",
-              height: "50px",
-              backgroundColor,
-            }}
-          ></div>
-        );
-      })}
+        {data.map((row, rowIndex) => (
+        <div 
+            key={rowIndex}>
+              {row.map((rgb, colIndex) => {
+                // convert from 1 to 255 to display colour
+                const r = rgb[0] * 255;
+                const g = rgb[1] * 255;
+                const b = rgb[2] * 255;
+                
+                // for each cell in a row
+                return (
+                  <div
+                  // TODO: weirdly tranposes the grid 
+                    key={`${rowIndex}-${colIndex}`}
+                    onClick={() => onCellClick(rowIndex, colIndex)}
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      // get colour from [rbg[0], rbg[1], rbg[2]]
+                      backgroundColor: `rgb(${r}, ${g}, ${b})`,
+                      border: "1px solid black",
+                      cursor: "pointer",
+                    }}
+                  ></div>
+                );
+              })}
+        </div>
+      ))}
     </div>
   );
 };
-
 export default Grid;
