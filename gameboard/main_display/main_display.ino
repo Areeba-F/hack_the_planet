94eb58c6c9cc5ec71c4479a6cc10ce90110b718c
Patrick Harvey <patrick.s.harvey1@gmail.com>
2025-02-17 01:33:52 -0700

    added support for larger map & dual-joystick

--- gameboard/main_display/main_display.ino
+++ gameboard/main_display/main_display.ino
@@ -8,43 +8,98 @@
 #define LED_W {1,1,1}
 #define LED_X {0,0,0}
 
+// define modulo for negative numbers
+#define MOD(a,b) ((((a)%(b))+(b))%(b))
+
 const int NUMROWS = 7;
 const int NUMCOLS_NORM = 5;
 const int NUMCOLS_INV = 2;
+const int STICK_X_LOW = 312;
+const int STICK_X_HIGH = 712;
+const int STICK_Y_LOW = 312;
+const int STICK_Y_HIGH = 712;
 
 struct {
-  int colours_norm[7][5][3];
-  int colours_inv[7][2][3];
+  bool colours_norm[NUMROWS][NUMCOLS_NORM][3];
+  bool colours_inv[NUMROWS][NUMCOLS_INV][3];
 } colourMatrix;
+
+bool fullMap[21][21][3];
+
+int stickX;
+int stickY;
+int xPos = 7;
+int yPos = 7;
+
+bool xLock = 0;
+bool yLock = 0;
 
 //pin assignments
 const int commonPins_norm[NUMROWS] = {42,40,38,36,30,34,32};
 const int ledPins_norm[NUMCOLS_NORM][3] = {{47,44,45},{2,3,4},{5,6,7},{8,9,10},{11,12,13}};
-
 const int commonPins_inv[NUMROWS] = {43,41,39,31,33,35,37};
 const int ledPins_inv[NUMCOLS_INV][3] = {{A0,A1,A2},{A8,A9,A10}};
+const int stickXPin = A14;
+const int stickYPin = A13;
+const int stick1PwrPin = 53;
+const int stick2PwrPin = 52;
 
 void pinSetup();
 void getColours();
 void displayGrid();
+void pollStick();
+void moveMapView(int, int);
 
 void setup() {
   Serial.begin(9600);
 
   pinSetup();
-  getColours();
+  TEST_setMap();
+  updateColours();
+  
 }
 
 void loop() {
   displayGrid();
-  
+  pollStick();
+}
+
+void pollStick() {
+  stickX = analogRead(stickXPin);
+  stickY = analogRead(stickYPin);
+
+  if ((stickX > STICK_X_LOW) && (stickX < STICK_X_HIGH) && (stickY > STICK_Y_LOW) && (stickY < STICK_Y_HIGH)) {
+    xLock = 0;
+    yLock = 0;
+  }
+
+  if (xLock == 0) {
+    if (stickX > STICK_X_HIGH) {
+      xLock = 1;
+      moveMapView(-1, 0);
+    } else if (stickX < STICK_X_LOW) {
+      xLock = 1;
+      moveMapView(1, 0);
+    }
+  }
+
+  if (yLock == 0) {
+    if (stickY > STICK_Y_HIGH) {
+      yLock = 1;
+      moveMapView(0, 1);
+    } else if (stickY < STICK_Y_LOW) {
+      yLock = 1;
+      moveMapView(0, -1);
+    }
+  }
+
 }
 
 
 void displayGrid() {
   for (int row = 0; row < NUMROWS; row++) {
     // go to next row of normal grid (common-anode)
-    digitalWrite(commonPins_norm[(row-1) % (NUMROWS-1)], HIGH);
+    digitalWrite(commonPins_norm[MOD(row-1,NUMROWS)], HIGH);
     for (int col = 0; col < NUMCOLS_NORM; col++) {
       digitalWrite(ledPins_norm[col][0], colourMatrix.colours_norm[row][col][0]);
       digitalWrite(ledPins_norm[col][1], colourMatrix.colours_norm[row][col][1]);
@@ -53,7 +108,7 @@ void displayGrid() {
     digitalWrite(commonPins_norm[row], LOW);
 
     // go to next row of inverted grid (common-cathode)
-    digitalWrite(commonPins_inv[(row-1) % (NUMROWS-1)], LOW);
+    digitalWrite(commonPins_inv[MOD(row-1,NUMROWS)], LOW);
     for (int col = 0; col < NUMCOLS_INV; col++) {
       digitalWrite(ledPins_inv[col][0], colourMatrix.colours_inv[row][col][0]);
       digitalWrite(ledPins_inv[col][1], colourMatrix.colours_inv[row][col][1]);
@@ -91,28 +146,55 @@ void pinSetup() {
     digitalWrite(ledPins_inv[i][1], HIGH);
     digitalWrite(ledPins_inv[i][2], HIGH);
   }
+
+  pinMode(stick1PwrPin, OUTPUT);
+  digitalWrite(stick1PwrPin, HIGH);
+  pinMode(stick2PwrPin, OUTPUT);
+  digitalWrite(stick2PwrPin, HIGH);
+  pinMode(stickXPin, INPUT);
+  pinMode(stickYPin, INPUT);
 }
 
-void getColours() {
-  //int colourArray[7][7][3] = {{LED_R,LED_G,LED_B,LED_C,LED_M,LED_Y,LED_W},{LED_G,LED_B,LED_C,LED_M,LED_Y,LED_W,LED_R},{LED_B,LED_C,LED_M,LED_Y,LED_W,LED_R,LED_G},{LED_C,LED_M,LED_Y,LED_W,LED_R,LED_G,LED_B},{LED_M,LED_Y,LED_W,LED_R,LED_G,LED_B,LED_C},{LED_Y,LED_W,LED_R,LED_G,LED_B,LED_C,LED_M},{LED_W,LED_X,LED_G,LED_X,LED_C,LED_X,LED_Y}};
+void moveMapView(int xMovement, int yMovement) {
+  xPos += xMovement;
+  yPos += yMovement;
   
+  if (xPos < 0) {
+    xPos = 0;
+  } else if (xPos > 14) {
+    xPos = 14;
+  }
+  if (yPos < 0) {
+    yPos = 0;
+  } else if (yPos > 14) {
+    yPos = 14;
+  }
+  updateColours();
+}
+
+void updateColours() {
   
   for (int i = 0; i < NUMROWS; i++) {
     for (int j = 0; j < NUMCOLS_NORM; j++) {
-//      colourMatrix.colours_norm[i][j][0] = colourArray[i][j][0];
-//      colourMatrix.colours_norm[i][j][1] = colourArray[i][j][1];
-//      colourMatrix.colours_norm[i][j][2] = colourArray[i][j][2];
-      colourMatrix.colours_norm[i][j][0] = 0;
-      colourMatrix.colours_norm[i][j][1] = 0;
-      colourMatrix.colours_norm[i][j][2] = 1;
+      colourMatrix.colours_norm[i][j][0] = fullMap[yPos+i][xPos+j][0];
+      colourMatrix.colours_norm[i][j][1] = fullMap[yPos+i][xPos+j][1];
+      colourMatrix.colours_norm[i][j][2] = fullMap[yPos+i][xPos+j][2];
+
     }
     for (int j = 0; j < NUMCOLS_INV; j++) {
-//      colourMatrix.colours_inv[i][j][0] = (int) !((bool) colourArray[i][NUMCOLS_NORM+j][0]);
-//      colourMatrix.colours_inv[i][j][1] = (int) !((bool) colourArray[i][NUMCOLS_NORM+j][1]);
-//      colourMatrix.colours_inv[i][j][2] = (int) !((bool) colourArray[i][NUMCOLS_NORM+j][2]);
-      colourMatrix.colours_inv[i][j][0] = 1;
-      colourMatrix.colours_inv[i][j][1] = 1;
-      colourMatrix.colours_inv[i][j][2] = 0;
+      colourMatrix.colours_inv[i][j][0] = !fullMap[yPos+i][xPos+NUMCOLS_NORM+j][0];
+      colourMatrix.colours_inv[i][j][1] = !fullMap[yPos+i][xPos+NUMCOLS_NORM+j][1];
+      colourMatrix.colours_inv[i][j][2] = !fullMap[yPos+i][xPos+NUMCOLS_NORM+j][2];
     }
+  }
+}
+
+void TEST_setMap() {
+  int colours[3][3] = {{1,0,0},{0,1,0},{0,0,1}};
+  
+  for (int i = 0; i < 21*21; i++) {
+    fullMap[(int) floor(i/21)][i%21][0] = colours[i%3][0];
+    fullMap[(int) floor(i/21)][i%21][1] = colours[i%3][1];
+    fullMap[(int) floor(i/21)][i%21][2] = colours[i%3][2];
   }
 }
